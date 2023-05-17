e57f2c853c50e3d310cf7290ca69d47fa5cca3d0
Holden Karau <holden@pigscanfly.ca>
2023-05-16 20:09:29 -0700

    Fix how we execute collectstatic.

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ mypy -p main -p pigscanfly
 ./manage.py makemigrations
 ./manage.py migrate
 ./manage.py validate_templates --ignore-app newsletter
-./manage.py collectstatic --no-input
 # Hack, for now.
 rm -rf ./cal-sync-magic
 cp -af ../cal-sync-magic ./
 rm -rf main/static/assets/images
 cp -af ../pcfweb-assets/images main/static/assets/
-docker buildx build --platform=linux/amd64,linux/arm64 -t holdenk/pcfweb:v0.9.10a . --push
+./manage.py collectstatic --no-input
+docker buildx build --platform=linux/amd64,linux/arm64 -t holdenk/pcfweb:v0.9.10c . --push
 kubectl apply -f deploy.yaml
