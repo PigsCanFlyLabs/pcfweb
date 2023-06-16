1085486fe862782836af8fa0a94eeb79c9ae91f9
Holden Karau <holden@pigscanfly.ca>
2023-06-15 17:02:00 -0700

    oops newsletter

--- build.sh
+++ build.sh
@@ -11,5 +11,5 @@ cp -af ../cal-sync-magic ./
 rm -rf main/static/assets/images
 cp -af ../pcfweb-assets/images main/static/assets/
 ./manage.py collectstatic --no-input
-docker buildx build --platform=linux/amd64,linux/arm64 -t holdenk/pcfweb:v0.9.10c . --push
+docker buildx build --platform=linux/amd64,linux/arm64 -t holdenk/pcfweb:v0.9.11b . --push
 kubectl apply -f deploy.yaml
